bb6c3078ccb67bb74cd485d7a1468eb42b6337a7
nasirali276433-alt <nasirali276433@gmail.com>
2026-06-08 00:15:30 +0500

    Modify success messages in form handling functions
    Updated success messages in showMessage and registerAccount functions.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-   function showMessage(event){
+ function showMessage(event){
     event.preventDefault();
 
     document.getElementById("message").innerHTML =
-    "Thank you! Your message has been submitted successfully.";
+    "Thank you! Your message has been sent successfully.";
 
     document.querySelector(".contact-form").reset();
 }
@@ -20,10 +20,32 @@ function registerAccount(event){
     event.preventDefault();
 
     document.getElementById("successMsg").innerHTML =
-    "Account Created Successfully!";
+    "Request Submitted Successfully!";
 
     setTimeout(() => {
         document.getElementById("registerForm").style.display = "none";
         document.getElementById("successMsg").innerHTML = "";
     }, 2000);
+
+    <div class="container">
+    <form class="login-form">
+        <h2>Login</h2>
+
+        <div class="input-box">
+            <input type="email" placeholder="Email Address" required>
+        </div>
+
+        <div class="input-box">
+            <input type="password" placeholder="Password" required>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <p class="register">
+            Don't have an account?
+            <a href="#">Register</a>
+        </p>
+    </form>
+</div>
+
 }
